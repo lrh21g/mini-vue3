f443d5982d0b0f5bef52d641474504b232ed8920
lrh <lingronghai@foxmail.com>
2024-12-27 14:42:30 +0800

    chore: 基础配置调整

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,11 +1,19 @@
+import path from 'node:path'
 import { defineWorkspace } from 'vitest/config'
 
 export default defineWorkspace([
   {
     test: {
-      name: 'packages',
+      globals: true,
       include: ['packages/**/*.test.{ts,js}'],
-      exclude: [],
+    },
+    resolve: {
+      alias: [
+        {
+          find: /@mini-vue3\/([\w-]*)/,
+          replacement: `${path.resolve(__dirname, 'packages')}/$1/src`,
+        },
+      ],
     },
   },
 ])

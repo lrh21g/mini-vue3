2f2522af049fa25a3c782056d5fee70353b58433
lrh <lingronghai@foxmail.com>
2025-01-06 23:46:19 +0800

    feat(shared): 新增 hasOwn 和 def 工具函数
    - 新增 hasOwn 函数用于判断对象是否拥有指定属性
    - 新增 def 函数用于动态定义和控制对象属性的行为

--- packages/shared/src/general.ts
+++ packages/shared/src/general.ts
@@ -36,3 +36,31 @@ export function isIntegerKey(key: unknown): boolean {
 // 使用 === 进行判断会不严格：当 newValue 和 oldValue 都为 NaN 时，为 false 。 eg: NaN === NaN => false ; +0 === -0 => false
 // 使用 Object.is 进行判断会更严格。 eg: Object.is(NaN, NaN) => true ; Object.is(+0, -0) => true
 export const hasChanged = (newValue: unknown, oldValue: unknown) => !Object.is(newValue, oldValue)
+
+const hasOwnProperty = Object.prototype.hasOwnProperty
+export function hasOwn(val: object, key: string | symbol): key is keyof typeof val {
+  return hasOwnProperty.call(val, key)
+}
+
+export function def(obj: object, key: string | symbol, value: any, writable = false): void {
+  Object.defineProperty(
+    obj,
+    key,
+    {
+      // 是否可配置。默认值为 false
+      // 当设置为 false 时，该属性的类型不能在数据属性和访问器属性（由 getter/setter 函数对描述的属性）之间更改，且不可被删除，且其描述符的其他属性也不能被修改。
+      // 如果是一个可写的数据描述符，则 value 可以被更改， writable 可以更改为 false 。
+      // eg : 使用 delete obj[key] 来删除该属性，或者修改它的 writable 或 enumerable 特性
+      configurable: true,
+      // 是否可枚举。默认值为 false
+      // 当且仅当该属性在对应对象的属性枚举中出现时，值为 true
+      // eg : 设置为 false，则不会出现在 for...in 循环或 Object.keys() 等方法的结果中。这个特性可以用于将一些内部属性隐藏起来，避免被列举出来。
+      enumerable: false,
+      // 是否可写。默认值为 false。
+      // 如果与属性相关联的值可以使用赋值运算符更改，则为 true
+      writable,
+      // 属性的值
+      value,
+    },
+  )
+}
